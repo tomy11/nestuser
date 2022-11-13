744943cda662dab87ef50ffe77deb8df129e0c21
tomy11 <ohnemotive74@gmail.com>
2022-11-13 12:17:07 +0700

    update config

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -6,17 +6,19 @@ import { UserModule } from './user.module';
 import { DepartmentModule } from './department.module';
 import { CompanyModule } from './company.module';
 import { AuthModule } from './auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
         type: 'mysql',
-        host: '127.0.0.1',
-        port: 3306,
-        username: 'root',
-        password: '123456',
-        database: 'happywork',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT) || 3306,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: true,
       }),
